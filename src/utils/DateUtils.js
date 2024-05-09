769e80c45357d0938b7723df327984c7d25f3250
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-05-09 02:02:19 -0400

    relative weeks logic created

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -1,6 +1,18 @@
-const currentWeek = () => {
-  const today = new Date();
-  return today;
+const getWeek = (num = 0) => {
+  const weekArr = [];
+  const date = new Date();
+  date.setDate(date.getDate() - num * 7);
+  date.setDate(date.getDate() - date.getDay() + 1);
+  while (weekArr.length < 7) {
+    weekArr.push({
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      day: date.getDate(),
+      dayOfWeek: date.getDay(),
+    });
+    date.setDate(date.getDate() + 1);
+  }
+  return weekArr;
 };
 
-export { currentWeek };
+export { getWeek };
